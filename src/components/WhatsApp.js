e6a80f38da4075632c3e391d636d8fc7821b6648
Your Name <you@example.com>
2023-01-02 15:34:13 +0500

    code update

--- src/components/WhatsApp.js
+++ src/components/WhatsApp.js
@@ -9,7 +9,7 @@ function WhatsApp() {
 
 <div className='whatsApp__button' data-aos="fade-up" data-aos-duration="800">
 
-<a target="_blank" href="https://wa.me/923024863105"  style={{backgroundColor: 'rgb(240, 240, 60)', width: '240px'}} className="btn px-4 py-3 text-white d-flex align-items-center justify-content-between">
+<a target="_blank" href=""  style={{backgroundColor: 'rgb(240, 240, 60)', width: '240px'}} className="btn px-4 py-3 text-white d-flex align-items-center justify-content-between">
            <span>Contact Now</span>
   <BsWhatsapp color='white' size={28} className="whatsApp__icon" />
          </a>
